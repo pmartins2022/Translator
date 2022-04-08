86507981ae83aa7c1af082236643fcf61feb28c4
Jose Aguiar <jose.aguiar-t22@upskill.pt>
2022-04-08 11:35:43 +0100

    Controllers e UIs UC3&4

--- src/main/java/controller/ControllerContarPalavras.java
+++ src/main/java/controller/ControllerContarPalavras.java
@@ -4,7 +4,14 @@ import model.Translator;
 
 public class ControllerContarPalavras
 {
+    Translator _translator;
+    public ControllerContarPalavras (Translator _translator)
+    {
+        this._translator=_translator;
+    }
 
-
-
+    public int contarPalavras ()
+    {
+        return _translator.contarPalavras();
+    }
 }
