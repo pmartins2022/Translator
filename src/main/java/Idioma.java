6cc19e4f05695ca90de343d4337240f42f17b112
MiguelLouzeiro <miguelant79@gmail.com>
2022-04-08 10:31:51 +0100

    Added class Idioma

---
+++ src/main/java/Idioma.java
@@ -0,0 +1,29 @@
+public class Idioma {
+
+    String nome;
+
+    public Idioma (String nome){
+        this.nome=nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Idioma: " + nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Idioma)) return false;
+        Idioma idioma = (Idioma) o;
+        return getNome().equals(idioma.getNome());
+    }
+}
