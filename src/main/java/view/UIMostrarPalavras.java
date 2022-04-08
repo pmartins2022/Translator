db52d4c335226f6d80cbf8601a52341a28d677d2
Jose Aguiar <jose.aguiar-t22@upskill.pt>
2022-04-08 10:58:14 +0100

    Controllers e UIs UC3&4

--- src/main/java/view/UIMostrarPalavras.java
+++ src/main/java/view/UIMostrarPalavras.java
@@ -1,5 +1,25 @@
 package view;
 
+import controller.ControllerMostrarPalavras;
+import model.Translator;
+
+import java.util.Scanner;
+
 public class UIMostrarPalavras
 {
+    Translator _translator;
+    Scanner lerTeclado = new Scanner (System.in);
+    ControllerMostrarPalavras _controller;
+
+    public UIMostrarPalavras (Translator _translator)
+    {
+        this._translator = _translator;
+        _controller = new ControllerMostrarPalavras (_translator);
+    }
+
+    public int mostrarPalavras()
+    {
+        return _controller.mostrarPalavras();
+    }
+
 }
